5aba4e3132b5164b870a1040b6ef5fa23e9b731a
Nicolas Wormser <nicolas.wormser@gmail.com>
2017-01-11 15:09:07 +0400

    WIP - Add vue-router

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,17 +1,28 @@
 <template>
   <div id="app">
     <cwf-heading id="cwf-heading"></cwf-heading>
-    <cwf-main id="cwf-main"></cwf-main>
+    <router-link v-for="post in postList" :to="post.route">post.title</router-link>
     <cwf-footer id="cwf-footer"></cwf-footer>
   </div>
 </template>
 
 <script>
+  import VueRouter from 'vue-router'
+  import CwfPost from './CwfHeading.vue'
   import CwfHeading from './CwfHeading.vue'
-  import CwfMain from './CwfMain.vue'
   import CwfFooter from './CwfFooter.vue'
 
+  const router = new VueRouter({
+    mode: 'history',
+    base: __dirname,
+    routes: [
+      {path: '/', name: 'home', component: CwfMain },
+      {path: '/:title', name: 'post', component: CwfPost} 
+    ]
+  });
+
   export default {
+    router,
     components: {
       CwfHeading,
       CwfMain,
--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
 const root = new Vue({
   ...App
 });
